192ba0957712bb90fec0baad7ba300b5f89d9bd2
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-26 21:10:11 +0700

    added scanned history modal

--- screen/ReaderScreen.js
+++ screen/ReaderScreen.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Modal, FlatList } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { FIREBASE_AUTH, FIRESTORE_DB } from '../firebase';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { collection, getDocs, query, where, orderBy, addDoc, serverTimestamp } from 'firebase/firestore';
 
 export default function ReaderScanner() {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const [scannedHistory, setScannedHistory] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
 
     useEffect(() => {
         const getBarCodeScannerPermissions = async () => {
@@ -32,8 +34,36 @@ export default function ReaderScanner() {
             data,
             timestamp: serverTimestamp(),
         });
+    };
 
+    const fetchScannedHistory = async () => {
+        const userUID = FIREBASE_AUTH.currentUser.uid; // Replace with your actual method to get UID
 
+        // Reference to the user's collection of scannedHistories
+        const userScannedHistoriesRef = collection(FIRESTORE_DB, `users/${userUID}/scannedHistories`);
+
+        // Query to get scanned histories, ordered by timestamp
+        const q = query(userScannedHistoriesRef, orderBy('timestamp', 'desc'));
+
+        // Get the documents
+        const querySnapshot = await getDocs(q);
+
+        // Extract the data from documents
+        const histories = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+
+        // Update the state with scanned history
+        setScannedHistory(histories);
+    };
+
+    const openModal = () => {
+        setModalVisible(true);
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
     };
 
     if (hasPermission === null) {
@@ -50,6 +80,31 @@ export default function ReaderScanner() {
                 style={StyleSheet.absoluteFillObject}
             />
             {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+            <Button title="Show Scanned History" onPress={openModal} />
+            {/* Button to show scanned history */}
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={closeModal}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <FlatList
+                            data={scannedHistory}
+                            keyExtractor={(item) => item.id}
+                            renderItem={({ item }) => (
+                                <View>
+                                    <Text>Type: {item.type}</Text>
+                                    <Text>Data: {item.data}</Text>
+                                    <Text>Timestamp: {item.timestamp.toDate().toString()}</Text>
+                                </View>
+                            )}
+                        />
+                        <Button title="Close" onPress={closeModal} />
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 }
@@ -57,7 +112,30 @@ export default function ReaderScanner() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#ffffff', // White background
         flexDirection: 'column',
         justifyContent: 'center',
+        padding: 16,
+    },
+    list: {
+        marginTop: 20,
+    },
+    item: {
+        backgroundColor: '#f0f0f0', // Light gray background for each item
+        padding: 10,
+        marginVertical: 8,
+        borderRadius: 8,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        elevation: 5,
     },
 });
