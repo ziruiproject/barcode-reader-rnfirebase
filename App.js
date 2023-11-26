4eb947cd409e0a29030abfc917be8165b9f366d2
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-26 18:02:08 +0700

    added firebase

--- App.js
+++ App.js
@@ -1,43 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
+import React from 'react'
+import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
+import { FIREBASE, FIREBASE_AUTH } from './firebase'
 
-async function onGoogleButtonPress() {
-  // Check if your device supports Google Play
-  await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-  // Get the users ID token
-  const { idToken } = await GoogleSignin.signIn();
-
-  // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
-}
-
-
-export default function App() {
-
-  GoogleSignin.configure({
-    webClientId: '670881354935-sk4prqtvea2peakce0ei9c7h4vd0elnq',
-  });
-
+const App = () => {
+  const auth = FIREBASE_AUTH
   return (
-    <View style={styles.container}>
-      <Button
-        title="Google Sign-In"
-        onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-      />
+    <View>
+      <TextInput>
+        halo
+      </TextInput>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
