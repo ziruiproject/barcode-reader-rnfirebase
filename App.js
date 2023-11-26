dfc740c5f14cfa958f9af50c4c8665fc7d12322c
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-26 18:24:48 +0700

    added login view

--- App.js
+++ App.js
@@ -1,15 +1,72 @@
-import React from 'react'
-import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
-import { FIREBASE, FIREBASE_AUTH } from './firebase'
+import { getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-const App = () => {
-  const auth = FIREBASE_AUTH
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your authentication logic here
+    const auth = getAuth()
+
+    console.log(`Logging in with email: ${email} and password: ${password}`);
+    // You might want to integrate with Firebase, Axios, or other authentication services
+  };
+
   return (
-    <View>
-      <TextInput>
-        halo
-      </TextInput>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
-export default App
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default LoginScreen;
